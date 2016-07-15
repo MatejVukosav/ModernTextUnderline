3ab0d5d97bafacdf5da166e94432a2894a478458
Matej Vukosav <mvukosav@cs.hr>
2016-07-15 16:17:42 +0200

    layour xml fix

--- app/src/main/java/vuki/com/moderntextunderline/MainActivity.java
+++ app/src/main/java/vuki/com/moderntextunderline/MainActivity.java
@@ -5,12 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
-     setContentView(  R.layout.activity_main );
-
+        setContentView( R.layout.activity_main );
     }
 }
